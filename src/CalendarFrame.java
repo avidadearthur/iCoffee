301ee40811ee93ff9933e4afbeea3ef219c1d5f4
arthur <arthurtquintao@gmail.com>
2022-03-28 00:44:17 +0200

    Update CalendarFrame to hold the main datetime setter

--- src/CalendarFrame.java
+++ src/CalendarFrame.java
@@ -1,9 +1,14 @@
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.toedter.calendar.JDateChooser;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 public class CalendarFrame extends JFrame{
 
@@ -12,6 +17,15 @@ public class CalendarFrame extends JFrame{
     private JDateChooser myDate;
     private JButton setDateButton;
     private String date;
+    private JPanel AlarmPanel1;
+    public JComboBox<String> comboBoxHour;
+    public JComboBox<String> comboBoxMin;
+    private JTextField textFieldVol;
+    private JComboBox<String> comboBoxTemp;
+    private JButton backButton;
+    private JButton saveButton;
+    private JPanel datePanel;
+    private int sessionID;
 
     public CalendarFrame(String title){
         super(title);
@@ -31,14 +45,69 @@ public class CalendarFrame extends JFrame{
 
     public void setCalendarUI() {
         myPanel = new JPanel();
-        myLabel = new JLabel();
+        myPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+
+        AlarmPanel1 = new JPanel();
+        AlarmPanel1.setLayout(new GridLayoutManager(6, 2, new Insets(0, 0, 0, 0), -1, -1));
+
+        myPanel.add(AlarmPanel1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        AlarmPanel1.setBorder(BorderFactory.createTitledBorder(null, "When and how do I want my coffee?", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+
         myDate = new JDateChooser();
         setDateButton = new JButton();
-        myLabel.setText("Select date");
-        setDateButton.setText("Set Date");
-        myPanel.add(myLabel);
-        myPanel.add(myDate);
-        myPanel.add(setDateButton);
+        myDate.setPreferredSize(new Dimension(200,25));
+
+        datePanel = new JPanel();
+        datePanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        datePanel.add(myDate, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+
+        AlarmPanel1.add(datePanel, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+
+
+        comboBoxHour = new JComboBox<>();
+        final DefaultComboBoxModel<String> defaultComboBoxModel1 = new DefaultComboBoxModel<>();
+        for (String s : Arrays.asList("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24")) {
+            defaultComboBoxModel1.addElement(s);
+        }
+
+        comboBoxHour.setModel(defaultComboBoxModel1);
+        AlarmPanel1.add(comboBoxHour, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label1 = new JLabel();
+        label1.setText("Minute");
+        AlarmPanel1.add(label1, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label2 = new JLabel();
+        label2.setText("Volume (cl)");
+        AlarmPanel1.add(label2, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        textFieldVol = new JTextField();
+        AlarmPanel1.add(textFieldVol, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        final JLabel label3 = new JLabel();
+        label3.setText("Temperature (°C)");
+        AlarmPanel1.add(label3, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        comboBoxTemp = new JComboBox<>();
+        final DefaultComboBoxModel<String> defaultComboBoxModel2 = new DefaultComboBoxModel<>();
+        defaultComboBoxModel2.addElement("COLD");
+        defaultComboBoxModel2.addElement("MEDIUM");
+        defaultComboBoxModel2.addElement("HOT");
+        comboBoxTemp.setModel(defaultComboBoxModel2);
+        AlarmPanel1.add(comboBoxTemp, new GridConstraints(4, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        saveButton = new JButton();
+        saveButton.setText("Save");
+        AlarmPanel1.add(saveButton, new GridConstraints(5, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label4 = new JLabel();
+        label4.setText("Hour");
+        AlarmPanel1.add(label4, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label5 = new JLabel();
+        label5.setText("Date");
+        AlarmPanel1.add(label5, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+
+        comboBoxMin = new JComboBox<>();
+        final DefaultComboBoxModel<String> defaultComboBoxModel3 = new DefaultComboBoxModel<>();
+        for (String s : Arrays.asList("00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60")) {
+            defaultComboBoxModel3.addElement(s);
+        }
+        comboBoxMin.setModel(defaultComboBoxModel3);
+        AlarmPanel1.add(comboBoxMin, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_NORTH, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+
     }
 
     public JLabel getMyLabel() {
@@ -54,9 +123,4 @@ public class CalendarFrame extends JFrame{
         return date;
     }
 
-/*    public static void main(String[] args) {
-        CalendarFrame ui= new CalendarFrame("Set date");
-        ui.setVisible(true);
-        ui.pack();
-    }*/
 }
